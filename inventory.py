b0c8f88e7896010f47796d429dc17a29853da9ae
ericedgari <ericedgari@hotmail.com>
2020-01-04 19:16:27 +0700

    scrollbar done

--- inventory.py
+++ inventory.py
@@ -1,6 +1,6 @@
 import sqlite3
 import tkinter as tk
-from tkinter import messagebox, ttk, HORIZONTAL, VERTICAL, RIGHT, Y, BOTTOM, X, W, NO
+from tkinter import messagebox, ttk
 
 #The size that we're going to work with
 
@@ -8,9 +8,7 @@ from tkinter import messagebox, ttk, HORIZONTAL, VERTICAL, RIGHT, Y, BOTTOM, X, 
 HEIGHT = 700
 WIDTH = 800
 
-#Variables
 
-# SEARCH = tk.StringVar()
 
 #METHOD
 def database():
@@ -56,31 +54,36 @@ class Title:
 
 class Widgets:
     def __init__(self,master):
+
+        self.SEARCH = tk.StringVar()
         frame_other_widgets = tk.Frame(master, bg="green")
         frame_other_widgets.place(relx=0.5, rely=0, relheight=1, relwidth=0.5)
 
         frame_inventory_list = tk.Frame(master, bg="black")
         frame_inventory_list.place(relx=0, rely=0, relheight=1, relwidth=0.45)
 
-        self.inventory_box = ttk.Treeview(frame_inventory_list,columns=("ProductID","Product Name","Product Qty","Product Price"),selectmode='extended')
+        self.scrollbary = tk.Scrollbar(master,orient='vertical')
+        self.inventory_box = ttk.Treeview(frame_inventory_list,columns=("ProductID","Product Name","Product Qty","Product Price"),selectmode='extended',yscrollcommand=self.scrollbary.set)
+        self.scrollbary.config(command=self.inventory_box.yview())
+        self.scrollbary.place(relx=0.45,rely=0.1,relheight=1)
         self.inventory_box.heading('ProductID',text='ID',anchor='w')
         self.inventory_box.heading('Product Name', text='Product Name', anchor='w')
         self.inventory_box.heading('Product Qty', text='Product Qty', anchor='w')
         self.inventory_box.heading('Product Price', text='Product Price', anchor='w')
         self.inventory_box.column('#0', stretch = 'no',minwidth=0, width=0)
-        self.inventory_box.column('#1', stretch = 'no',minwidth=0,width=20)
-        self.inventory_box.column('#2', stretch='no', minwidth=0, width=120)
-        self.inventory_box.column('#3', stretch='no', minwidth=0, width=120)
-        self.inventory_box.column('#4', stretch='no', minwidth=0, width=120)
+        self.inventory_box.column('#1', stretch = 'no',minwidth=0,width=0)
+        self.inventory_box.column('#2', stretch='no', minwidth=0, width=126)
+        self.inventory_box.column('#3', stretch='no', minwidth=0, width=127)
+        self.inventory_box.column('#4', stretch='no', minwidth=0, width=127)
         self.inventory_box.place(relx=0, rely=0.1, relheight=1, relwidth=1)
         self.DisplayData()
 
 
-        self.search_bar = tk.Entry(frame_other_widgets, font=25)
+        self.search_bar = tk.Entry(frame_other_widgets, font=25,textvariable=self.SEARCH)
         self.search_bar.place(relx=0, rely=0.1, relheight=0.05, relwidth=0.70)
 
         self.search_button = tk.Button(frame_other_widgets, text="Search",
-                                  command=lambda: get_search_entry(search_bar.get()))
+                                  command=lambda:self.search())
         self.search_button.place(relx=0.75, rely=0.1, relheight=0.05, relwidth=0.23)
 
         self.add_button = tk.Button(frame_other_widgets, text="Add", command=lambda: self.showAddNew())
@@ -90,7 +93,7 @@ class Widgets:
                                        command=lambda: self.refresh())
         self.refresh_button.place(relx=0.75, rely=0.22, relheight=0.05, relwidth=0.23)
 
-        self.delete_button = tk.Button(frame_other_widgets, text="Delete",
+        self.delete_button = tk.Button(frame_other_widgets, text="Delete",command= lambda:self.delete()
                                        )
         self.delete_button.place(relx=0.75, rely=0.28, relheight=0.05, relwidth=0.23)
 
@@ -149,6 +152,34 @@ class Widgets:
         for i in self.inventory_box.get_children():
             self.inventory_box.delete(i)
         self.DisplayData()
+
+    def search(self):
+        if self.SEARCH.get() != "":
+            self.inventory_box.delete(*self.inventory_box.get_children())
+            database()
+            cursor.execute("SELECT * FROM `product` WHERE `product_name` LIKE ?", ('%' + str(self.SEARCH.get()) + '%',))
+            fetch = cursor.fetchall()
+            for data in fetch:
+                self.inventory_box.insert('', 'end', values=(data))
+            cursor.close()
+            conn.close()
+
+    def delete(self):
+        if not self.inventory_box.selection():
+            print("ERROR")
+        else:
+            result = messagebox.askquestion('Inventory System', 'Are you sure you want to delete this record?',
+                                              icon="warning")
+            if result == 'yes':
+                curItem = self.inventory_box.focus()
+                contents = (self.inventory_box.item(curItem))
+                selecteditem = contents['values']
+                self.inventory_box.delete(curItem)
+                database()
+                cursor.execute("DELETE FROM `product` WHERE `product_id` = %d" % selecteditem[0])
+                conn.commit()
+                cursor.close()
+                conn.close()
 
 
 
